191012c6bb8d24184813b86ce173c3522ba6556a
alekh08 <137406174+alekh08@users.noreply.github.com>
2026-01-27 12:10:46 +0530

    assignment3: create http request module

---
+++ http-req.js
@@ -0,0 +1,10 @@
+// Create a node server that listens on port 3000 and responds with "Hello, Node!" for any incoming http request.
+
+const http=require('node:http');                // without node: prefix it can be ambigous whether it refers to built in file sys module or npm package
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello, Node!');
+});
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
